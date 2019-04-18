58f5c6b8a5293bf1c1f042e57a8fff2c9aba1f5a
Ujjwal Batra <ujjwalbatra2509@gmail.com>
2019-04-18 19:37:32 +1000

    input validation completed

---
+++ reception_pi/util/input_validation.py
@@ -0,0 +1,61 @@
+from validate_email import validate_email
+import re
+
+
+class InputValidation(object):
+
+    @staticmethod
+    def validate_email(email: str):
+        """
+        checks whether the email address is valid or not
+
+        Args:
+            email: email address to be validated
+
+        Returns:
+            boolean: a boolean specifying whether the email is valid or not
+        """
+
+        is_valid = validate_email(email)
+
+        return is_valid
+
+    @staticmethod
+    def validate_username(username: str):
+        """
+        checks whether the username is of length 5 minimum, and contains only a-z (lowercase characters)
+        or A-Z (uppercase characters) or 0-9 (digits). **no special characters allowed**
+
+        Args:
+            username: username to be validated
+
+        Returns:
+            boolean: a boolean specifying whether the username is valid or not
+        """
+
+        is_valid = re.search("[0-9a-zA-Z]{5,}", username)
+
+        if is_valid:
+            return True
+
+        return False
+
+    @staticmethod
+    def validate_password(password: str):
+        """
+        checks whether the username is of length 6 minimum, and contains only a-z (lowercase characters),
+        A-Z (uppercase characters), 0-9 (digits) or special characters (supports only: * # $ @ ! % ^)
+
+        Args:
+            password: password to be validated
+
+        Returns:
+            boolean: a boolean specifying whether the username is valid or not
+        """
+
+        is_valid = re.search("[0-9a-zA-Z*#$@!%^]{6,}", password)
+
+        if is_valid:
+            return True
+
+        return False
