500ef38a5c3e3995d63bfda18a805984e3d53c06
Ujjwal Batra <ujjwalbatra2509@gmail.com>
2019-05-22 13:32:11 +1000

    404 added and session created

--- admin_portal/app.py
+++ admin_portal/app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template, request, redirect, url_for
+import json
+
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_session import Session
 
 app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'memcached'
+app.config['SECRET_KEY'] = 'super secret key'
+sess = Session()
 
 
 @app.route('/')
 def homepage():
-    return redirect(url_for('dashboard'))
+    return redirect(url_for('login'))
 
 
 @app.route('/check-credentials/', methods=["POST"])
@@ -13,15 +19,17 @@ def check_credentials():
     error = None
     if request.method == 'POST':
         if request.form.get('username', None) != 'admin' or request.form.get('password', None) != 'admin':
-            error = 'Invalid Credentials. Please try again.'
+            error = 'Invalid Credentials! Please try again.'
         else:
             return redirect(url_for('dashboard'))
-    return render_template('login.html', error=error)
+
+    flash(error, "danger")
+    return redirect(url_for('login'))
 
 
 @app.route('/login/')
 def login():
-    return render_template('login.html', error=None)
+    return render_template('login.html')
 
 
 @app.route('/dashboard/')
@@ -42,6 +50,11 @@ def search():
 @app.route('/list-books/')
 def list_books():
     return render_template("list-books.html")
+
+
+@app.errorhandler(404)
+def page_not_found():
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
