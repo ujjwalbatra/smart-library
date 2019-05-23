4809e694d5d38b9944b8e568090cc8eb8bbae01f
Ujjwal Batra <ujjwalbatra2509@gmail.com>
2019-05-23 11:10:17 +1000

    debugging session authentication

--- admin_portal/app.py
+++ admin_portal/app.py
@@ -16,7 +16,9 @@ app.register_blueprint(site)
 
 @app.route('/')
 def homepage():
-    if "authenticated" in session and session['authenticated'] is not True:
+    if "authenticated" not in session:
+        return redirect(url_for('login'))
+    elif session['authenticated'] is not True:
         return redirect(url_for('login'))
 
     return redirect(url_for(dashboard))
@@ -24,7 +26,9 @@ def homepage():
 
 @app.route('/check-credentials/', methods=["POST"])
 def check_credentials():
-    if "authenticated" in session and session['authenticated'] is not True:
+    if "authenticated" not in session:
+        return redirect(url_for('login'))
+    elif session['authenticated'] is not True:
         return redirect(url_for('login'))
 
     error = None
@@ -46,7 +50,9 @@ def login():
 
 @app.route('/dashboard/')
 def dashboard():
-    if "authenticated" in session and "authenticated" in session and session['authenticated'] is not True:
+    if "authenticated" not in session:
+        return redirect(url_for('login'))
+    elif session['authenticated'] is not True:
         return redirect(url_for('login'))
 
     return render_template("dashboard.html")
@@ -54,14 +60,19 @@ def dashboard():
 
 @app.route('/add-book/')
 def add():
-    if "authenticated" in session and session['authenticated'] is not True:
+    if "authenticated" not in session:
         return redirect(url_for('login'))
+    elif session['authenticated'] is not True:
+        return redirect(url_for('login'))
+
     return render_template("add-book.html")
 
 
 @app.route('/add-book-verification/', methods=["POST", "GET"])
 def add_book_verification():
-    if "authenticated" in session and session['authenticated'] is not True:
+    if "authenticated" not in session:
+        return redirect(url_for('login'))
+    elif session['authenticated'] is not True:
         return redirect(url_for('login'))
 
     if request.method == 'POST':
@@ -92,7 +103,9 @@ def add_book_verification():
 
 @app.route('/search-book/')
 def search_book():
-    if "authenticated" in session and session['authenticated'] is not True:
+    if "authenticated" not in session:
+        return redirect(url_for('login'))
+    elif session['authenticated'] is not True:
         return redirect(url_for('login'))
 
     return render_template("search-book.html")
@@ -100,7 +113,9 @@ def search_book():
 
 @app.route('/search/', methods=["POST", "GET"])
 def search():
-    if "authenticated" in session and session['authenticated'] is not True:
+    if "authenticated" not in session:
+        return redirect(url_for('login'))
+    elif session['authenticated'] is not True:
         return redirect(url_for('login'))
 
     if request.method == 'POST':
@@ -119,7 +134,9 @@ def search():
 
 @app.route('/list-books/')
 def list_books():
-    if "authenticated" in session and session['authenticated'] is not True:
+    if "authenticated" not in session:
+        return redirect(url_for('login'))
+    elif session['authenticated'] is not True:
         return redirect(url_for('login'))
 
     return render_template("list-books.html")
@@ -135,7 +152,9 @@ def logout():
 
 @app.route('/delete-book/<id>', methods=["POST"])
 def delete_book(id):
-    if "authenticated" in session and session['authenticated'] is not True:
+    if "authenticated" not in session:
+        return redirect(url_for('login'))
+    elif session['authenticated'] is not True:
         return redirect(url_for('login'))
 
     # remove triangular brackets that comes with the request
